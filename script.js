1bcd35eb22647b673433df246c4dc74a16282198
Avishek Saha <barshan23@gmail.com>
2017-08-19 12:50:51 +0530

    Separated the js from the html

---
+++ script.js
@@ -0,0 +1,33 @@
+var page = 1;
+function cardContent(gist) {
+	return '<div class="col s12 m6 l4">\
+				<div class="card-panel hoverable">\
+					<div class="card-content">\
+						<span class="card-title">User: '+gist['user']+'</span>\
+						<p class="truncate">Description:'+(gist['description']?
+							gist['description']:"No Description")+'</p> \
+					</div>\
+					<div class="card-action"><a href='+gist['html_url']+'>Learn More</a></div>\
+				</div>\
+			</div>';
+}
+function ajx(page) {
+	$.ajax({
+		url:'https://api.github.com/gists/public?page='+page+'&per_page=12', 
+		success:function(result) {
+			//console.log(result);
+			//console.log(result[0]['commits_url']);
+			$.each(result, function(i, gist){
+				//console.log(gist['html_url']);
+				$(".row").append(cardContent(gist));
+			});
+		}
+	});
+}
+$(document).ready(function(){
+	ajx(page);
+	$("button").click(function(){
+		page += 1;
+		ajx(page);
+	});
+});
