5a39472dd4733d2305f52a205b6d0b8b3dd93f5e
Avishek Saha <barshan23@gmail.com>
2017-08-20 22:36:17 +0530

    Fixed time format for the updated time

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var page = 1;
 function cardContent(gist) {
+	var time = gist['updated_at'];
+	time = time.slice(0,time.search("T"));
+	console.log(time);
+	time = time.split('-');
+	console.log(time);
 	return '<div class="col s12 m6 l4">\
 				<div class="card-panel hoverable">\
 					<div class="card-content">\
 						<span class="card-title">User: '+gist['user']+'</span><br />\
-						<span >	Updated at: '+gist['updated_at']+'</span>\
+						<span >	Updated at: '+time[2]+'/'+time[1]+'/'+time[0]+'</span>\
 						<p class="truncate">Description:'+(gist['description']?
 							gist['description']:"No Description")+'</p> \
 					</div>\
@@ -16,7 +21,7 @@ function ajx(page) {
 	$.ajax({
 		url:'https://api.github.com/gists/public?page='+page+'&per_page=12', 
 		success:function(result) {
-			//console.log(result);
+			console.log(result);
 			//console.log(result[0]['commits_url']);
 			$.each(result, function(i, gist){
 				//console.log(gist['html_url']);
